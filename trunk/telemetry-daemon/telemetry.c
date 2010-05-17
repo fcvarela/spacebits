6976497c926ed5629878f2b480b2c4a080b4c321
filipe <filipe@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-05-17 15:47:44 +0000

    Added compile-time switch to read from file in debug mode
    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@33 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/telemetry-daemon/telemetry.c
+++ trunk/telemetry-daemon/telemetry.c
@@ -17,15 +17,22 @@
 #define SERIAL_PORT "/dev/cu.usbserial-A6007x6A"
 #define SERIAL_SPD 115200
 
+#define DEBUG
+
+#ifndef DEBUG
 pthread_t command_thread;
 pthread_mutex_t serial_port_lock = PTHREAD_MUTEX_INITIALIZER;
-
 int serial_port;
+#endif
+
 broker_server_t *broker_server;
 sapo_broker_t *broker;
 
 void catch_quit(int signal) {
+	#ifndef DEBUG
 	close(serial_port);
+	#endif
+	
 	printf("Closing serial port\n");
 	exit(0);
 }
@@ -35,11 +42,13 @@ int main(int argc, char **argv) {
 	signal(SIGINT, catch_quit);
 	signal(SIGQUIT, catch_quit);
 	
+	#ifndef DEBUG
 	// init serial port
 	if ((serial_port = setup_port()) == -1) {
 		perror("setup_port");
 		return 1;
 	}
+	#endif
 	
 	// init broker connection
 	char hostname[] = "127.0.0.1";
@@ -54,11 +63,14 @@ int main(int argc, char **argv) {
 	packet_loop();
 
 	// detach command thread
+	#ifndef DEBUG
 	pthread_create(&command_thread, NULL, (void *)&command_loop, NULL);
+	#endif
 	
 	return 0;
 }
 
+#ifndef DEBUG
 int setup_port(void) {
 	int fd;
 	struct termios newtio;
@@ -84,27 +96,50 @@ int setup_port(void) {
 	
 	return(fd);
 }
+#endif
 
 void packet_loop(void) {
 	sensor_data_t packet;
+	#ifndef DEBUG
 	char c;
+	#endif
 	
 	while (1) {
-		pthread_mutex_lock(&serial_port_lock);
-		read(serial_port, &c, 1);
-		if (c != 'A') {
-			pthread_mutex_unlock(&serial_port_lock);
-			continue;
-		}
-		
-		read(serial_port, &packet, sizeof(packet));
-		read(serial_port, &c, 1);
-		if (c != 'Z') {
-			pthread_mutex_unlock(&serial_port_lock);
-			continue;
-		}
-		
-		pthread_mutex_unlock(&serial_port_lock);
+		#ifndef DEBUG
+		    pthread_mutex_lock(&serial_port_lock);
+		    read(serial_port, &c, 1);
+		    if (c != 'A') {
+			    pthread_mutex_unlock(&serial_port_lock);
+			    continue;
+		    }
+		    
+		    read(serial_port, &packet, sizeof(packet));
+    		read(serial_port, &c, 1);
+    		if (c != 'Z') {
+    			pthread_mutex_unlock(&serial_port_lock);
+    			continue;
+    		}
+
+    		pthread_mutex_unlock(&serial_port_lock);
+		#else
+		    packet.voltage = 1024;
+            packet.current = 1024;
+            packet.bearing = 1024;
+            packet.humidity = 1024;
+            packet.dust_density = 1024;
+            packet.rtc.tm_hour = 12;
+            packet.rtc.tm_min = 30;
+            packet.rtc.tm_sec = 59;
+            packet.imu.gx = 1024;
+            packet.imu.gy = 1024;
+            packet.imu.ax = 1024;
+            packet.imu.ay = 1024;
+            packet.imu.az = 1024;
+            packet.scp.raw_pressure = 2048;
+            packet.scp.raw_temperature = 2048;
+            sleep(1); //simulate a little locking delay (locking read on serial port)
+        #endif
+
 		// push packet to telemetry topic
 		unsigned int retval = broker_publish(broker, TELEMETRY_TOPIC, (char *)&packet, sizeof(sensor_data_t));
 		fprintf(stderr, "Published packet: %u\n", retval);
