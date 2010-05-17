73819ea1cf394995bed141de40c7cf05a61941ca
celso <celso@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-05-17 18:10:42 +0000

    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@36 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/website/www/api.php
+++ trunk/website/www/api.php
@@ -2,7 +2,7 @@
 
 $post=file_get_contents('php://input');
 
-file_put_contents("/tmp/spacepost.xml",print_r($post,true));
+file_put_contents("/tmp/spacepost.xml",print_r($post,true),FILE_APPEND);
 
 echo "<result>ok</result>\n";
 
