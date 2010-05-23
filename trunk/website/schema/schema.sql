e06adaad77e0d57aa49f52754330fc9a75f2f0b4
celso <celso@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-05-23 14:42:38 +0000

    - Database schemas
    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@58 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

---
+++ trunk/website/schema/schema.sql
@@ -0,0 +1,38 @@
+create table articles (
+  id char(60),
+  body blob,
+  title char(80),
+  change int,
+  user char(20), PRIMARY KEY(id)
+  );
+
+create table users (
+  user char(60),
+  password char(40),PRIMARY KEY(user)
+  );
+
+create table data (
+  change int,
+  power_current real,
+  power_voltage real,
+  pressure real,
+  temperature real,
+  humidity real,
+  dust_density real,
+  lat real,
+  lon real,
+  alt real,
+  bear real,
+  imu_gx real, 
+  imu_gy real, 
+  imu_ax real, 
+  imu_ay real, 
+  imu_az real, PRIMARY KEY(change)
+  );
+
+create table sms (
+  change int,
+  source char(80),
+  message char(160), PRIMARY KEY(change)
+  );
+
