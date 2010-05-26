1252d760eabc17b000ffbbed8db7ef84fb196a7c
celso <celso@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-05-26 22:40:03 +0000

    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@84 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/website/repo/update_repo.sh
+++ trunk/website/repo/update_repo.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+cd /servers/spacebits/repo
 export COPYFILE_DISABLE
 export COPY_EXTENDED_ATTRIBUTES_DISABLE
 ./dpkg-deb-fat -b SpacebitsMobile
-
+rm -fr /tmp/repo
+mkdir /tmp/repo
+mv SpacebitsMobile.deb /tmp/repo
+cp dpkg-gettext.pl /Library/Perl/
+chmod 755 /Library/Perl/dpkg-gettext.pl
+cd /tmp/repo
+./dpkg-scanpackages -m . /dev/null >Packages
+bzip2 Packages
+cd ..
+rm -fr /servers/spacebits/www/repo
+mv repo /servers/spacebits/www/repo
