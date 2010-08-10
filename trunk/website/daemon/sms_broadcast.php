4457ffe2e0eddebaa1a8088535306c138c292d3d
celso <celso@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-08-10 23:13:19 +0000

    - Broadcast SMS
    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@137 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/website/daemon/sms_broadcast.php
+++ trunk/website/daemon/sms_broadcast.php
@@ -9,15 +9,18 @@ error_reporting(1);
 
 $api=new Spacebits_API;
 
-if($sms=$api->lastSMS()) {
+$balloon_id=2;
+
+
+if($sms=$api->lastSMS($balloon_id)) {
   $stamp=md5($sms['lon'].$sms['lat']);
-  if(@file_get_contents("/tmp/lastspacesms")!=$stamp) {
+  if(@file_get_contents("/tmp/lastspacesms".$balloon_id)!=$stamp) {
     echo "There's an update. Broadcasting...\n";
-    $body="Lat: ".$sms['lat']." Lon: ".$sms['lon']." alt: ".$sms['alt']." nsats: ".$sms['nsats']." http://maps.google.com/maps?q=".$sms['lat'].",".$sms['lon'];
+    $body="BalloonId: ".$balloon_id." Lat: ".$sms['lat']." Lon: ".$sms['lon']." alt: ".$sms['alt']." nsats: ".$sms['nsats']." http://maps.google.com/maps?q=".$sms['lat'].",".$sms['lon'];
     foreach(split(" ",RECEPTORS) as $n) {
       echo "Broadcasting to ".$n."\n";
       exec("/servers/spacebits/daemon/send_sms.pl ".$n." '".$body."'");
       }
-    file_put_contents("/tmp/lastspacesms",$stamp);
+    file_put_contents("/tmp/lastspacesms".$balloon_id,$stamp);
     }
   }
