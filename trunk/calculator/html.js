b6bcc6026016e68f4f17eb155042e67fd0e07273
cvalente <cvalente@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2010-10-21 17:37:38 +0000

    set payload when not defined
    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@160 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/calculator/html.js
+++ trunk/calculator/html.js
@@ -8,6 +8,7 @@ function ballon_setup(){
         dropdown.add(option, null);
     }
     dropdown.onchange = function(){return balloon_set_model(dropdown);};
+    //set default
     dropdown.options[dropdown.options.length-3].selected = 'selected';
     balloon_set_model(dropdown);
 }
@@ -40,6 +41,10 @@ function balloon_set_model(select){
     var lift = document.getElementById('balloon_lift');
     var burst = document.getElementById('balloon_burst');
 
+    if( "" === payload.value ){
+        payload.value = balloon['Payload (gr)']
+    }
+
     button.onclick = function(){calculate(balloon, payload, speed, lift, burst); return false;};
 
     return;
@@ -53,7 +58,7 @@ function calculate(balloon, payload, speed, lift, burst){
     _burst = parseFloat(burst.value);
 
     if( isNaN(_payload)){
-        report_error('Must specify the payload.');
+        return report_error('Must specify the payload.');
     }else{
         //first see whether there is only one unknown value
         if( !isNaN(_lift) && isNaN(_speed) && isNaN(_burst)){
@@ -66,11 +71,15 @@ function calculate(balloon, payload, speed, lift, burst){
             _lift = nozzle_lift_from_burst_altitude(balloon, _burst);
             _speed = speed_from_nozzle_lift(balloon, _payload, _lift);
         }else{
-            report_error('Overdetermined. Only one of speed, lift or burst may be defined');
+            return report_error('Overdetermined. Only one of speed, lift or burst may be defined');
         }
-        speed.value = _speed.toFixed(2);
-        lift.value = _lift.toFixed(0);
-        burst.value = _burst.toPrecision(3);
+        if(_lift >= _payload){
+            speed.value = _speed.toFixed(2);
+            lift.value = _lift.toFixed(0);
+            burst.value = _burst.toPrecision(3);
+        }else{
+            return report_error('Insufficient lift for the payload.');
+        }
     }
 }
 
