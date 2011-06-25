6d69e3d8b2c9dc88934c850469bec94025902eec
filipe <filipe@a2c5a79c-5111-4c67-8fa9-ec7ffeda9481>
2011-06-25 10:24:39 +0000

    Fixed duplicate include
    git-svn-id: svn://svn.softwarelivre.sapo.pt/spacebits@187 a2c5a79c-5111-4c67-8fa9-ec7ffeda9481

--- trunk/telemetry-android/telemetry.c
+++ trunk/telemetry-android/telemetry.c
@@ -10,12 +10,6 @@
 #include <sys/select.h>
 #include <signal.h>
 #include <unistd.h>
-#include <termios.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
 #include <jni.h>
 
 #include "telemetry.h"
